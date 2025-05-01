4c44a62aeecdbc2e5368b0111e2380674491f559
Audrey Paleczny <87346850+AudreyPaleczny@users.noreply.github.com>
2025-04-30 20:44:18 -0500

    april 30 lecture

--- Makefile
+++ Makefile
@@ -1,5 +1,23 @@
 CXX = g++ # C++
 CC = gcc # C compiler 
-CXXFLAGS = -Wall - Wextra -std=c++11 -pedantic -I./include $(shell pkg-config --cflags glfw3) # extra options you want to give to your compiler b4 compiling it
+CXXFLAGS = -Wall -Wextra -std=c++11 -pedantic -I./include $(shell pkg-config --cflags glfw3) # extra options you want to give to your compiler b4 compiling it
 CFLAGS = -I./include # I means include & -I is for header files 
 LIBS = $(shell pkg-config --libs glfw3) # libraries
+
+TARGET = output 
+OBJECTS = src/main.o src/glad.o
+
+build: $(TARGET)
+
+$(TARGET): $(OBJECTS) # output depends on the files
+# $^ is getting objects and $@ is getting target
+	$(CXX) $^ -o $@ $(LIBS) 
+
+.PHONY: clean
+clean:
+	rm -f src/*.o output
+
+run:
+	./output
+	
+	
---
+++ src/main.cpp
@@ -0,0 +1,59 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTENT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTENT_VERSION_MINOR, 3); // so version 3.3
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    // make our window resizeable
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfGetProcAddress))
+    {
+        std::cout << "Failed Init Glad" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(1920, 1080, "Text", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window); // if you have multiple windows open, you don't know which one to draw into --> this tells the program
+
+    // glViewport(0, 0, 1920, 1080); // window holds together all vars in opengl context but isn't the rendering 
+    // viewport is what opengl is actually rendering to 
+    // window is the frame and viewport is the screen with the analogy of a computer 
+    
+    // when adding y, it goes downward
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+
+}
+
+// tell the program new window size if user resizes window
+// anything you can't guarantee when it happens --> callback function (i.e. when the user presses space, jump)
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // update with the new width and new height
+    glViewport(0, 0, width, height);
+}
