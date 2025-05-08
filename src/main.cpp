a3185de672b9b8fe004d6d9fc6de83935266f827
Audrey Paleczny <87346850+AudreyPaleczny@users.noreply.github.com>
2025-05-07 19:53:28 -0500

    may 7 lecture of drawing a triangle

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,20 @@
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+
 int main()
 {
     // Initialize GLFW
@@ -42,8 +56,74 @@ int main()
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // start our rendering pipeline
+    float vertices[] = 
+    {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
+    // create our buffer
+    unsigned int VBO;
+    // printf("%d\n", VBO); // 0
+    glGenBuffers(1, &VBO); // 1 is how many we're creating
+    // printf("%d\n", VBO); // 1
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    // create our attrib buffer 
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+
+    // send data to the buffer
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // what we're sending, how many we're sending, pointer to data itself, static.. means we'll send the data once
+    // sent to buffer but not GPU yet 
+
+    // create our vertex shader
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    // need shadersource for turning the global var strings above into something comprehensive for OpenGL
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); // just compiling 1 shader rn
+    glCompileShader(vertexShader); // turns strings into machine code
+
+    // create our fragment shader
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL); 
+    glCompileShader(fragmentShader);
+
+    // create shader program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    // attach our shaders
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // tell OpenGL to use it
+    glUseProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // specify our vertex attributes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // check doc for param explanations
+    // since we have a vec3, the size of the attributes should be 3
+    glEnableVertexAttribArray(0); // which slot we're enabling 
+
     while (!glfwWindowShouldClose(window))
     {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // r g b opacity
+        glClear(GL_COLOR_BUFFER_BIT); // makes the screen the above color
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
