e567b4c3e0c123442fda90fb2454d4aa22876c6c
Audrey Paleczny <87346850+AudreyPaleczny@users.noreply.github.com>
2025-05-07 19:55:05 -0500

    added comment

--- src/main.cpp
+++ src/main.cpp
@@ -120,6 +120,7 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // r g b opacity
         glClear(GL_COLOR_BUFFER_BIT); // makes the screen the above color
 
+        // these two lines make no diff for us b/c we only have one shader program
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
